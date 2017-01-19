41d3693f28d7349af81d28648b33094b86864686
ITZhouXu <374952705@qq.com>
2017-01-19 13:03:51 +0800

    init commit

---
+++ RegistWebProject/src/com/bodhixu/ResultEntity.java
@@ -0,0 +1,24 @@
+package com.bodhixu;
+
+public class ResultEntity {
+
+	private int resultCode;
+	private String resultMsg;
+	
+	public int getResultCode() {
+		return resultCode;
+	}
+	
+	public void setResultCode(int resultCode) {
+		this.resultCode = resultCode;
+	}
+	
+	public String getResultMsg() {
+		return resultMsg;
+	}
+	
+	public void setResultMsg(String resultMsg) {
+		this.resultMsg = resultMsg;
+	}
+	
+}
